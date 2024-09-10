6b32125095ab6b69c9bc13746f23fa3e6441a53e
Jun Won Kim <rlajunwon0531@gmail.com>
2024-09-10 14:01:53 +0900

    category.html에서 question.html로 이동
    URL에 value, number 파라미터 삽입

--- static/js/category.js
+++ static/js/category.js
@@ -2,26 +2,22 @@ window.onload = function() {
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-
     const value = urlParams.get('value');
-
-    // console.log(value);
 
     const valueElement = document.getElementById('category-title');
     if (valueElement) {
         valueElement.innerText = `${value}`;
     }
-};
 
-document.addEventListener('DOMContentLoaded', function() {
     const questionRows = document.querySelectorAll('.question-row');
-    
+
     questionRows.forEach(row => {
         row.addEventListener('click', function() {
+
             const questionNumber = this.querySelector('.question-number').textContent;
-            const questionTitle = this.querySelector('.question-title').textContent;
-            location.href = 'question.html';
-            // alert(`문제 번호: ${questionNumber}\n문제 제목: ${questionTitle}`);
+
+            console.log(`Value: ${value}, 문제 번호: ${questionNumber}`);
+            location.href = `question.html?value=${value}&&number=${questionNumber}`
         });
     });
-});
+};
