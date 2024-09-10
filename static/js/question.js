ba03ede1657f4eec9a3f28e5fb9fc220e27256e0
Jun Won Kim <rlajunwon0531@gmail.com>
2024-09-10 21:47:55 +0900

    question.html 페이지 css 수정 및 js 정답/오답 확인

--- static/js/question.js
+++ static/js/question.js
@@ -14,9 +14,30 @@ window.onload = function() {
     const menuCategoryElement = document.getElementById('menu-category');
     if (menuCategoryElement) {
         menuCategoryElement.addEventListener('click', function() {
-
-            const url = `category.html?value=${encodeURIComponent(value)}`;
+            const url = `category.html?value=${value}`;
             location.href = url;
+        });
+    }
+
+    let answer = 9999; // 정답 
+
+    const form = document.querySelector('.answer-form');
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            const input = document.querySelector('.answer-input');
+            const feedback = document.getElementById('feedback');
+
+            if (input && feedback) {
+                if (input.value == answer) {
+                    feedback.textContent = "맞았습니다.";
+                    feedback.className = "message correct";
+                } else {
+                    feedback.textContent = `${input.value} : 틀렸습니다`;
+                    feedback.className = "message wrong";
+                }
+            }
         });
     }
 };
